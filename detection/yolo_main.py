2396e39c3390d28d7bd426dd1a6a9ac05cc0a377
Jakub Jaszczak <jakub.jaszczak2@gmail.com>
2026-04-11 19:31:23 +0200

    feat: improve saving results - yolo

--- detection/yolo_main.py
+++ detection/yolo_main.py
@@ -1,37 +1,36 @@
-from enum import Enum, auto
+from datetime import datetime
 from pathlib import Path
 
 from ultralytics import YOLO
 
-class ModelVersion(Enum):
-    YOLO11n = auto()
-    YOLOv8n = auto()
-    YOLOv8s = auto()
-
-
-# TODO take from yaml
 PROJECT_ROOT_DIR = Path(__file__).parent.parent
 CONFIDENCE_THRESHOLD = 0.7
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     weights_path = PROJECT_ROOT_DIR.joinpath("runs/detect/train4/weights/best.pt")
     model = YOLO(weights_path)
-    # for img in PROJECT_ROOT_DIR.joinpath("datasets", "licence_plate","images", "test").iterdir():
-    #     results = model.predict(PROJECT_ROOT_DIR.joinpath("datasets", "licence_plate","images", "test"))
-    #     break
-
-    results = model.predict(PROJECT_ROOT_DIR.joinpath("datasets", "licence_plate", "images", "test"), conf=CONFIDENCE_THRESHOLD)
+    base_model_name = model.ckpt["train_args"].get("model").split(".")[0]
+    results = model.predict(
+        source=PROJECT_ROOT_DIR.joinpath("datasets", "licence_plate", "images", "test"),
+        conf=CONFIDENCE_THRESHOLD,
+        project=PROJECT_ROOT_DIR.joinpath("detection", "results", base_model_name.upper()),  # The base directory
+        name=datetime.now().strftime("%y%m%d"),  # Sub dir
+        exist_ok=True,
+    )
 
     for r in results:
-        print(r.boxes.data)
         r_np = r.cpu().numpy()
-        # First bbox is the one with the highest conf, for now only try the first one
         try:
-            bbox = r_np.boxes.xyxy[0]
+            bboxes = r_np.boxes.xyxy
         except IndexError as e:
             print(e)
             continue
-        x1, y1, x2, y2 = bbox.astype(int)
-        cropped_img = r.orig_img[y1:y2, x1:x2]
-        filename = Path(r.path).name
-        r.save_crop(r.save_dir, filename)
+        out_dir = Path(r.save_dir) / Path(r.path).stem
+        r.save(
+            out_dir.joinpath("results.jpg").as_posix(),
+        )
+        r.save_txt(Path(out_dir) / f"results.txt", save_conf=True)
+        for i, bbox in enumerate(bboxes):
+            r.save_crop(out_dir, f"{i}.jpg")
+
     print("Finished")
